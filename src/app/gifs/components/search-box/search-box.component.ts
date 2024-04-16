716eba715a0f3b8ecd97e90ae00674dec3e5d564
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-04-16 13:31:31 -0500

    seccion 7 video 92. GifsService

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -9,8 +10,11 @@ export class SearchBoxComponent {
   @ViewChild('txtTagInput')
   tagInput!: ElementRef<HTMLInputElement>;
 
+  constructor(private gifsService: GifsService) {}
   searchTag() {
     const newTag = this.tagInput.nativeElement.value;
+    this.gifsService.searchTag(newTag);
     console.log({ newTag });
+    this.tagInput.nativeElement.value = '';
   }
 }
