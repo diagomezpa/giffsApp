51a5002ddadb37d83de6c29bb4c4a3a3230a7203
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-04-16 13:00:26 -0500

    seccion 7 91. @ViewChild - Referencia al HTML

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,16 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'gifs-search-box',
+  templateUrl: './search-box.component.html',
+  styleUrl: './search-box.component.css',
+})
+export class SearchBoxComponent {
+  @ViewChild('txtTagInput')
+  tagInput!: ElementRef<HTMLInputElement>;
+
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+    console.log({ newTag });
+  }
+}
